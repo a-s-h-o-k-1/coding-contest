27ff47ddda72bcc65c6f0f696a7f2861ecbf06f6
Vignesh <vigneshch1234@gmail.com>
2026-09-04 11:59:36 +0530

    Intial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/coding/codingcontest/CodingContestApplication.java
@@ -0,0 +1,14 @@
+package com.coding.codingcontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingContestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("program is working");
+        SpringApplication.run(CodingContestApplication.class, args);
+    }
+
+}
